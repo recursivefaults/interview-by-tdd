b0641ff9c19db180a44f0d680ffb25469fedd8a8
Edward Kerr <edd.kerr@smashingboxes.com>
2018-09-28 13:28:50 -0400

    add test for dynamic output validation

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,8 +4,11 @@ describe FizzBuzz do
   it "should output 100 things if given 100 as an input" do
     expect(subject.run(100).count).to be 100
   end
-  xit "should output 5 things if given 5 as an input" do
+
+  it "should output 5 things if given 5 as an input" do
+    expect(subject.run(5).count).to be 5
   end
+
   xit "should give 'fizz' for every multiple of 3" do
   end
   xit "should give 'buzz' for every multiple of 5" do
