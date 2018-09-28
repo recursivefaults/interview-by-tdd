ecf2976a40cee8835f35a9f4669f5efaec747d93
Edward Kerr <edd.kerr@smashingboxes.com>
2018-09-28 13:39:54 -0400

    add code to past fizz case

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,17 @@
 class FizzBuzz
+  FIZZ = 'fizz'
+
   def run(length)
     result = []
     length.times do |n|
-      result << n
+      result << does_fizz?(n)
     end
     result
+  end
+
+  private
+
+  def does_fizz?(n)
+    n % 3 == 0 ? FIZZ : n
   end
 end
